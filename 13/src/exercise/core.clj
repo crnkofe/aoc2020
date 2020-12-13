f618385d10d169d14e463768c79c6ea80947cc9a
Simon Mihevc <simon.mihevc@gmail.com>
2020-12-13 17:22:37 +0100

    Refactor some code. Didn't manage to get part 2 done.

--- 13/src/exercise/core.clj
+++ 13/src/exercise/core.clj
@@ -11,7 +11,33 @@
     )]
     {:start-time (Integer/parseInt (first data)) :bus-lines (map #(Integer/parseInt %) (filter #(not= % "x") (str/split (nth data 1) #",")))}
   )
-)        
+)
+
+(defn filter-x [data processed-data previous]
+  (if (empty? data)
+     processed-data
+    (if (= previous (first data))
+      (filter-x (rest data) processed-data (first data))
+      (filter-x (rest data) (conj processed-data (first data)) (first data))
+    )
+  )
+)
+
+(defn load-timetable-pause
+  "First line is earlies departure time, second line lists busses"
+  [filename]
+  (let [data (with-open [rdr (clojure.java.io/reader filename)]
+               (reduce conj [] (line-seq rdr)))
+         filtered-timetable (filter-x (str/split (nth data 1) #",") [] nil)
+         offsets (map #(hash-map (Integer/parseInt (first %)) (nth % 1)) (filter #(not= (first %) "x") (map #(list % (.indexOf filtered-timetable %)) filtered-timetable)))
+]
+    {
+      :start-time (Integer/parseInt (first data)) 
+      :bus-lines (map #(Integer/parseInt %) (filter #(not= % "x") (str/split (nth data 1) #",")))
+      :offsets (reduce merge offsets)
+    }
+  )
+)
 
 (defn find-earliest-departure [ts bus-lines]
   (let [candidates (filter #(= 0 (nth % 1)) (map #(list % (mod ts %)) bus-lines))]
@@ -22,12 +48,14 @@
   )
 )
 
+
 (defn -main
   "Advent of Code. Day 13"
   [& args]
-  (let [timetable (load-timetable "input.txt")
+  (let [timetable (load-timetable "sample.txt")
+        timetable-pause (load-timetable-pause "sample.txt")
         earliest-departure (find-earliest-departure (get timetable :start-time) (get timetable :bus-lines))
  		waiting-time (- (get earliest-departure :start-time) (get timetable :start-time))]
-  (println "Solution of part 1:" (* waiting-time (first (get earliest-departure :bus-lines))))
+    (println "Solution of part 1:" (* waiting-time (first (get earliest-departure :bus-lines))))
   )
 )
